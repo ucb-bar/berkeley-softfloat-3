2c6e86c6eae4d82283af7c7af686f3e86052af1a
tharvik <tharvik@users.noreply.github.com>
2019-01-19 14:36:57 +0100

    fix inlining

--- source/s_countLeadingZeros16.c
+++ source/s_countLeadingZeros16.c
@@ -37,6 +37,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdint.h>
 #include "platform.h"
 
+uint_fast8_t softfloat_countLeadingZeros16( uint16_t a );
+
 #ifndef softfloat_countLeadingZeros16
 
 #define softfloat_countLeadingZeros16 softfloat_countLeadingZeros16
